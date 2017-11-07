5cf087eaf9d069f43eb736148f952a8d72b189cb
Donald <don.mennerich@nyu.edu>
2017-11-07 15:58:57 -0500

    sorrting digital object list by component identifier

--- app/controllers/ComposersController.scala
+++ app/controllers/ComposersController.scala
@@ -5,6 +5,7 @@ import javax.inject._
 
 import scala.concurrent.{ Future, ExecutionContext }
 import scala.concurrent.duration._
+import scala.collection.immutable.ListMap
 
 import play.api.Configuration
 import play.api._
@@ -13,6 +14,7 @@ import play.api.libs.ws._
 import play.api.http.HttpEntity
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import play.api.i18n.I18nSupport
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
@@ -27,27 +29,47 @@ case class Archiveit(title: String, extent: String, display_url: String)
 @Singleton
 class ComposersController @Inject()(config: Configuration)(cc: ControllerComponents)(ws: WSClient)(implicit ec:ExecutionContext) extends AbstractController(cc) {
   
-  implicit val archiveitWrites: Writes[Archiveit] = (
-  (JsPath \ "title").write[String] and
-  (JsPath \ "extent").write[String] and
-  (JsPath \ "display_url").write[String])(unlift(Archiveit.unapply))
-
   val aspaceUrl = config.get[String]("aspaceUrl")
   val rootUrl = config.get[String]("rootUrl")
 
   def archiveIt(identifier: String) = Action.async {
+    
+    implicit val archiveitWrites: Writes[Archiveit] = (
+      (JsPath \ "title").write[String] and
+      (JsPath \ "extent").write[String] and
+      (JsPath \ "display_url").write[String])(unlift(Archiveit.unapply))
+
     ws.url(aspaceUrl + "archiveit?resource_id=" + identifier).get().map { response => 
       val json = Json.parse(response.body)
       val archiveIt = new Archiveit(json("title").as[String], 
         json("extent").as[String], 
-        rootUrl + "summary/" + identifier)
-      
+        rootUrl + "summary/" + identifier) 
       Ok(Json.toJson(archiveIt))
     }
+
   }
+
+
+  def testws() = Action.async { implicit request: Request[AnyContent] =>
+    
+
+      val r = ws.url(aspaceUrl + "summary?resource_id=mss.460")
+      println(r.headers)
+      r.get().map { response => 
+      println(response.headers) 
+      Ok("ok")
+    }
+  }
+
   def summary(identifier: String) = Action.async { implicit request: Request[AnyContent] =>
 
-  	ws.url(aspaceUrl + "summary?resource_id=" + identifier).get().map { response =>
+  	val request = ws.url(aspaceUrl + "summary?resource_id=" + identifier)
+    request.withHttpHeaders("charset" -> "utf-8")
+
+    var doss = Map[String, JsObject]()
+
+    request.get().map { response =>
+
   		val json = Json.parse(response.body)
   		val version = json("version").as[String]
   		val resourceTitle = json("resource_title").as[String]
@@ -57,8 +79,16 @@ class ComposersController @Inject()(config: Configuration)(cc: ControllerCompone
   		val biog = json("bioghist").as[String]
   		val summary = new Summary(version, resourceTitle,resourceId, eadLocation, scope, biog)
   		val dos = json("digital_objects").as[Vector[JsObject]]
-  		Ok(views.html.summary(summary, dos, rootUrl))
+
+      for(doo <- dos) {
+        doss = doss + ((doo \ "component_id").as[String] -> doo)
+      }
+
+  		Ok(views.html.summary(summary, ListMap(doss.toSeq.sortWith(_._1 < _._1):_*), rootUrl))
   	}
+
+
+
   }
 
   def detail(cuid: String) = Action.async { implicit request: Request[AnyContent] =>
@@ -79,8 +109,6 @@ class ComposersController @Inject()(config: Configuration)(cc: ControllerCompone
   		Ok(views.html.detail(dao))
   	}
   }
-
-
 
   def index() = Action {
     Ok(views.html.index())
